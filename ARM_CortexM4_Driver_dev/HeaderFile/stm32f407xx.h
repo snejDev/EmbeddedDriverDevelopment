2511887140ae76e8adfc6e18f1ce00df0e8ffe3c
Snehal Jayachander <66051560+snejDev@users.noreply.github.com>
2022-10-23 05:02:12 +0530

    Update stm32f407xx.h

--- ARM_CortexM4_Driver_dev/HeaderFile/stm32f407xx.h
+++ ARM_CortexM4_Driver_dev/HeaderFile/stm32f407xx.h
@@ -6,6 +6,7 @@
  */
 
 #include <stdint.h>
+#include <stddef.h>
 
 #define _vo					volatile;
 
@@ -63,23 +64,6 @@
 #define SYSCFG_BASEADDR		(APB2PERIPH_BASE+0x3800)	//SYSCFG Baseaddress
 #define EXTI_BASEADDR		(APB2PERIPH_BASE+0x3C00)	//EXTI Baseaddress
 
-//GPIO Register Definitions
-#define GPIOA				(GPIO_RegDef*)GPIOA_BASEADDR	//Storing Typecasted base address of each GPIOA port Base address
-#define GPIOB				(GPIO_RegDef*)GPIOB_BASEADDR	//Storing Typecasted base address of each GPIOB port Base address
-#define GPIOC				(GPIO_RegDef*)GPIOC_BASEADDR	//Storing Typecasted base address of each GPIOC port Base address
-#define GPIOD				(GPIO_RegDef*)GPIOD_BASEADDR	//Storing Typecasted base address of each GPIOD port Base address
-#define GPIOE				(GPIO_RegDef*)GPIOE_BASEADDR	//Storing Typecasted base address of each GPIOE port Base address
-#define GPIOF				(GPIO_RegDef*)GPIOF_BASEADDR	//Storing Typecasted base address of each GPIOF port Base address
-#define GPIOG				(GPIO_RegDef*)GPIOG_BASEADDR	//Storing Typecasted base address of each GPIOG port Base address
-#define GPIOH				(GPIO_RegDef*)GPIOH_BASEADDR	//Storing Typecasted base address of each GPIOH port Base address
-#define GPIOI				(GPIO_RegDef*)GPIOI_BASEADDR	//Storing Typecasted base address of each GPIOI port Base address
-
-//RCC Clock Register macro definition
-#define RCCCLK				(RCC_RegDef*)RCC_BASEADDR;		//Storing Typecasted base address of each RCC clock Base address
-
-//Clock enable and disable macros
-
-
 //GPIO register definition structure
 typedef struct
 {								//Address offsets
@@ -95,70 +79,82 @@ typedef struct
 }GPIO_RegDef_t;
 
 typedef struct
-{									//Address offsets
-	_vo uint32_t RCC_CR;			//0x00
-	_vo	uint32_t RCC_PLLCFGR;		//0x04
-	_vo	uint32_t RCC_CFGR;			//0x08
-	_vo	uint32_t RCC_CIR;			//0x0C
-	_vo	uint32_t RCC_AHB1RSTR;		//0x10
-	_vo	uint32_t RCC_AHB2RSTR;		//0x14
-	_vo uint32_t RCC_AHB3RSTR;		//0x18
+{								//Address offsets
+	_vo uint32_t CR;			//0x00
+	_vo	uint32_t PLLCFGR;		//0x04
+	_vo	uint32_t CFGR;			//0x08
+	_vo	uint32_t CIR;			//0x0C
+	_vo	uint32_t AHB1RSTR;		//0x10
+	_vo	uint32_t AHB2RSTR;		//0x14
+	_vo uint32_t AHB3RSTR;		//0x18
 	_vo uint32_t RESERVED;			//0x1C
-	_vo uint32_t RCC_APB1RSTR;		//0x20
-	_vo uint32_t RCC_APB2RSTR;		//0X24
+	_vo uint32_t APB1RSTR;		//0x20
+	_vo uint32_t APB2RSTR;		//0X24
 	_vo uint32_t RESERVED1[2];		//0x28,0x2C
-	_vo uint32_t RCC_AHB1ENR;		//0x30
-	_vo uint32_t RCC_AHB2ENR;		//0x34
-	_vo uint32_t RCC_AHB3ENR;		//0x38
+	_vo uint32_t AHB1ENR;		//0x30
+	_vo uint32_t AHB2ENR;		//0x34
+	_vo uint32_t AHB3ENR;		//0x38
 	_vo uint32_t RESERVED2;			//0x3C
-	_vo uint32_t RCC_APB1ENR;		//0x40
-	_vo uint32_t RCC_APB2ENR;		//0x44
+	_vo uint32_t APB1ENR;		//0x40
+	_vo uint32_t APB2ENR;		//0x44
 	_vo uint32_t RESERVED3[2];		//0X48,0x4C
-	_vo uint32_t RCC_AHB1LPENR;		//0x50
-	_vo uint32_t RCC_AHB2LPENR;		//0x54
-	_vo uint32_t RCC_AHB3LPENR;		//0x58
+	_vo uint32_t AHB1LPENR;		//0x50
+	_vo uint32_t AHB2LPENR;		//0x54
+	_vo uint32_t AHB3LPENR;		//0x58
 	_vo uint32_t RESERVED4;			//0x5C
-	_vo uint32_t RCC_APB1LPENR;		//0x60
-	_vo uint32_t RCC_APB2LPENR;		//0x64
+	_vo uint32_t APB1LPENR;		//0x60
+	_vo uint32_t APB2LPENR;		//0x64
 	_vo uint32_t RESERVED5[2];		//0x68,0x6C
-	_vo uint32_t RCC_BDCR;			//0x70
-	_vo uint32_t RCC_CSR;			//0x74
+	_vo uint32_t BDCR;			//0x70
+	_vo uint32_t CSR;			//0x74
 	_vo uint32_t RESERVED6[2];		//0x78,0x7C
-	_vo uint32_t RCC_SSCGR;			//0x80
-	_vo uint32_t RCC_PLLI2SCFGR;	//0x84
+	_vo uint32_t SSCGR;			//0x80
+	_vo uint32_t PLLI2SCFGR;	//0x84
 	//_vo uint32_t RCC_PLLSAICFGR;
 	//_vo uint32_t RCC_DCKCFGR;
 }RCC_RegDef_t;
 
+//GPIO Register Definitions
+#define GPIOA				((GPIO_RegDef_t*)GPIOA_BASEADDR)	//Storing Typecasted base address of each GPIOA port Base address
+#define GPIOB				((GPIO_RegDef_t*)GPIOB_BASEADDR)	//Storing Typecasted base address of each GPIOB port Base address
+#define GPIOC				((GPIO_RegDef_t*)GPIOC_BASEADDR)	//Storing Typecasted base address of each GPIOC port Base address
+#define GPIOD				((GPIO_RegDef_t*)GPIOD_BASEADDR)	//Storing Typecasted base address of each GPIOD port Base address
+#define GPIOE				((GPIO_RegDef_t*)GPIOE_BASEADDR)	//Storing Typecasted base address of each GPIOE port Base address
+#define GPIOF				((GPIO_RegDef_t*)GPIOF_BASEADDR)	//Storing Typecasted base address of each GPIOF port Base address
+#define GPIOG				((GPIO_RegDef_t*)GPIOG_BASEADDR)	//Storing Typecasted base address of each GPIOG port Base address
+#define GPIOH				((GPIO_RegDef_t*)GPIOH_BASEADDR)	//Storing Typecasted base address of each GPIOH port Base address
+#define GPIOI				((GPIO_RegDef_t*)GPIOI_BASEADDR)	//Storing Typecasted base address of each GPIOI port Base address
+
+//RCC Clock Register macro definition
+#define RCCCLK				((RCC_RegDef_t*)RCC_BASEADDR) 	//Storing Typecasted base address of each RCC clock Base address
+
 //Peripheral clocks enable and disable
 
 //GPIOx Peripheral clock enable
-#define GPIOA_PCLK_EN()		RCCCLK->RCC_AHB1ENR |= (0x01<<0);
-#define GPIOB_PCLK_EN()		RCCCLK->RCC_AHB1ENR |= (0x01<<1);
-#define GPIOC_PCLK_EN()		RCCCLK->RCC_AHB1ENR |= (0x01<<2);
-#define GPIOD_PCLK_EN()		RCCCLK->RCC_AHB1ENR |= (0x01<<3);
-#define GPIOE_PCLK_EN()		RCCCLK->RCC_AHB1ENR |= (0x01<<4);
-#define GPIOF_PCLK_EN()		RCCCLK->RCC_AHB1ENR |= (0x01<<5);
-#define GPIOG_PCLK_EN()		RCCCLK->RCC_AHB1ENR |= (0x01<<6);
-#define GPIOH_PCLK_EN()		RCCCLK->RCC_AHB1ENR |= (0x01<<7);
-#define GPIOI_PCLK_EN()		RCCCLK->RCC_AHB1ENR |= (0x01<<8);
+#define GPIOA_PCLK_EN()		(RCCCLK->AHB1ENR |= (0x01<<0))
+#define GPIOB_PCLK_EN()		(RCCCLK->AHB1ENR |= (0x01<<1))
+#define GPIOC_PCLK_EN()		(RCCCLK->AHB1ENR |= (0x01<<2))
+#define GPIOD_PCLK_EN()		(RCCCLK->AHB1ENR |= (0x01<<3))
+#define GPIOE_PCLK_EN()		(RCCCLK->AHB1ENR |= (0x01<<4))
+#define GPIOF_PCLK_EN()		(RCCCLK->AHB1ENR |= (0x01<<5))
+#define GPIOG_PCLK_EN()		(RCCCLK->AHB1ENR |= (0x01<<6))
+#define GPIOH_PCLK_EN()		(RCCCLK->AHB1ENR |= (0x01<<7))
+#define GPIOI_PCLK_EN()		(RCCCLK->AHB1ENR |= (0x01<<8))
 
 //GPIOx Peripheral clock disable
-#define GPIOA_PCLK_DI()		RCCCLK->RCC_AHB1ENR &= ~(0x01<<0);
-#define GPIOB_PCLK_DI()		RCCCLK->RCC_AHB1ENR &= ~(0x01<<1);
-#define GPIOC_PCLK_DI()		RCCCLK->RCC_AHB1ENR &= ~(0x01<<2);
-#define GPIOD_PCLK_DI()		RCCCLK->RCC_AHB1ENR &= ~(0x01<<3);
-#define GPIOE_PCLK_DI()		RCCCLK->RCC_AHB1ENR &= ~(0x01<<4);
-#define GPIOF_PCLK_DI()		RCCCLK->RCC_AHB1ENR &= ~(0x01<<5);
-#define GPIOG_PCLK_DI()		RCCCLK->RCC_AHB1ENR &= ~(0x01<<6);
-#define GPIOH_PCLK_DI()		RCCCLK->RCC_AHB1ENR &= ~(0x01<<7);
-#define GPIOI_PCLK_DI()		RCCCLK->RCC_AHB1ENR &= ~(0x01<<8);
-
+#define GPIOA_PCLK_DI()		(RCCCLK->AHB1ENR &= ~(0x01<<0))
+#define GPIOB_PCLK_DI()		(RCCCLK->AHB1ENR &= ~(0x01<<1))
+#define GPIOC_PCLK_DI()		(RCCCLK->AHB1ENR &= ~(0x01<<2))
+#define GPIOD_PCLK_DI()		(RCCCLK->AHB1ENR &= ~(0x01<<3))
+#define GPIOE_PCLK_DI()		(RCCCLK->AHB1ENR &= ~(0x01<<4))
+#define GPIOF_PCLK_DI()		(RCCCLK->AHB1ENR &= ~(0x01<<5))
+#define GPIOG_PCLK_DI()		(RCCCLK->AHB1ENR &= ~(0x01<<6))
+#define GPIOH_PCLK_DI()		(RCCCLK->AHB1ENR &= ~(0x01<<7))
+#define GPIOI_PCLK_DI()		(RCCCLK->AHB1ENR &= ~(0x01<<8))
 
 #define ENABLE		1
 #define DISABLE		0
 #define SET			1
 #define RESET 		0
-
 
 #endif /* STM32F407XX_H_ */
